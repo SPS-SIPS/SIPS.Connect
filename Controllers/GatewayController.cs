87082dda38fdab46b529bb99174320be4c64d77c
Abdulshakur Ahmed <62094798+hanadderia@users.noreply.github.com>
2025-03-17 10:53:11 +0300

    Update: Introduced Constants instead of strings Add: Added Payment Return Endpoint

--- Controllers/GatewayController.cs
+++ Controllers/GatewayController.cs
@@ -4,6 +4,8 @@ using SIPS.ISO20022.Interfaces;
 using SIPS.ISO20022.Models.DTOs;
 using SIPS.ISO20022.Models.DTOs.CB;
 using Microsoft.AspNetCore.Mvc;
+using static SIPS.Connect.Helpers.APIResponseRenderer;
+using static SIPS.Connect.Constants;
 namespace SIPS.Connect.Controllers;
 [ApiController]
 [Produces("application/json")]
@@ -12,38 +14,50 @@ public class GatewayController(
     IJsonAdapter jsonAdapter,
     IOutgoingVerificationHandler verificationService,
     IOutgoingTransactionHandler transactionService,
-    IOutgoingTransactionStatusHandler transactionStatusService
+    IOutgoingTransactionStatusHandler transactionStatusService,
+    IOutgoingReturnTransactionHandler returnTransactionService
     ) : ControllerBase
 {
     private readonly IJsonAdapter _jsonAdapter = jsonAdapter;
     private readonly IOutgoingVerificationHandler _verificationService = verificationService;
     private readonly IOutgoingTransactionHandler _transactionService = transactionService;
     private readonly IOutgoingTransactionStatusHandler _transactionStatusService = transactionStatusService;
+    private readonly IOutgoingReturnTransactionHandler _returnTransactionService = returnTransactionService;
 
     [HttpPost("Verify")]
     public async Task<ActionResult> VerifyPayee([FromBody] JsonObject body, CancellationToken ct)
     {
-        JsonObject md = _jsonAdapter.Transform(body, "VerificationRequest");
+        JsonObject md = _jsonAdapter.Transform(body, VerificationRequest);
         var query = _jsonAdapter.ToObject<VerificationRequestDto>(md);
         var response = await _verificationService.HandleAsync(query, ct);
-        return APIResponseRenderer.GenerateAdminMessage(response, _jsonAdapter, "VerificationResponse");
+        return GenerateAdminMessage(response, _jsonAdapter, VerificationResponse);
     }
 
     [HttpPost("Payment")]
     public async Task<ActionResult> MakePayment([FromBody] JsonObject body, CancellationToken ct)
     {
-        JsonObject md = _jsonAdapter.Transform(body, "PaymentRequest");
+        JsonObject md = _jsonAdapter.Transform(body, PaymentRequest);
         var query = _jsonAdapter.ToObject<PaymentRequestDto>(md);
         var response = await _transactionService.HandleAsync(query, ct);
-        return APIResponseRenderer.GenerateAdminMessage(response, _jsonAdapter, "PaymentResponse");
+        return GenerateAdminMessage(response, _jsonAdapter, PaymentResponse);
     }
 
     [HttpPost("Status")]
     public async Task<ActionResult> GetStatus([FromBody] JsonObject body, CancellationToken ct)
     {
-        JsonObject md = _jsonAdapter.Transform(body, "StatusRequest");
+        JsonObject md = _jsonAdapter.Transform(body, StatusRequest);
         var query = _jsonAdapter.ToObject<StatusRequestDto>(md);
         var response = await _transactionStatusService.HandleAsync(query, ct);
-        return APIResponseRenderer.GenerateAdminMessage(response, _jsonAdapter, "PaymentResponse");
+        return GenerateAdminMessage(response, _jsonAdapter, PaymentResponse);
+    }
+
+    [HttpPost("Return")]
+    public async Task<ActionResult> GetReturn([FromBody] JsonObject body, CancellationToken ct)
+    {
+        JsonObject md = _jsonAdapter.Transform(body, ReturnRequest);
+        var query = _jsonAdapter.ToObject<ReturnPaymentRequestDto>(md);
+        Console.WriteLine("md.OriginalTxId" + query.OriginalTxId);
+        var response = await _returnTransactionService.HandleAsync(query, ct);
+        return GenerateAdminMessage(response, _jsonAdapter, ReturnResponse);
     }
 }
